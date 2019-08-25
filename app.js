4c4f1dfc78037be63b79d26b7f7f40c7fde60f54
kaforad <kaforad@gmail.com>
2019-08-25 11:45:16 +0100

    Create remove command,Create Remove Function

---
+++ app.js
@@ -0,0 +1,43 @@
+const yargs = require('yargs');
+const notes = require('./notes.js');
+yargs.command({
+    command :'remove',
+    describe:'Remove object by Title',
+    builder:{
+        'title':{
+            'demandOption':true,
+            'string':true,
+            'describe':'Data Key Title',
+        },
+    },
+    handler:function(argv){
+      notes.removeNote(argv.title);
+
+    }
+
+});
+
+
+yargs.command({
+    command :'add',
+    describe:'Add a new note',
+    builder:{
+        'title':{
+            'demandOption':true,
+            'string':true,
+            'describe':'Note Title',
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler:function(argv){
+        notes.addNote(argv.title, argv.body)
+
+    }
+
+});
+
+yargs.parse();
